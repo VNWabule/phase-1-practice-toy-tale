f0321c895c22988fb33b2d2c7a4809afd0e80328
VNWabule <vicwabule@gmail.com>
2025-03-25 18:31:25 +0300

    done

--- src/index.js
+++ src/index.js
@@ -13,3 +13,72 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.getElementById("toy-collection");
+  const toyForm = document.querySelector(".add-toy-form");
+
+  
+  function fetchToys() {
+    fetch("http://localhost:3000/toys")
+      .then(response => response.json())
+      .then(toys => toys.forEach(toy => renderToy(toy)));
+      console.log(data);
+  }
+
+  function renderToy(toy) {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>
+    `;
+    
+    const likeButton = card.querySelector(".like-btn");
+    likeButton.addEventListener("click", () => updateLikes(toy, card));
+    
+    toyCollection.appendChild(card);
+  }
+
+  
+  toyForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const image = event.target.image.value;
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ name, image, likes: 0 })
+    })
+      .then(response => response.json())
+      .then(toy => renderToy(toy));
+    
+    toyForm.reset();
+  });
+
+  
+  function updateLikes(toy, card) {
+    const newLikes = toy.likes + 1;
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({ likes: newLikes })
+    })
+      .then(response => response.json())
+      .then(updatedToy => {
+        toy.likes = updatedToy.likes;
+        card.querySelector("p").textContent = `${updatedToy.likes} Likes`;
+      });
+  }
+
+  fetchToys();
+});
